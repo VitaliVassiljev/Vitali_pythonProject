2bf16fd7213db50de7b44535da9e1105b1ebc05b
VitaliVassiljev <110185897+VitaliVassiljev@users.noreply.github.com>
2022-12-02 23:39:32 +0200

    New file

---
+++ venv/Practice/Test_1_Simple_actions.py
@@ -0,0 +1,18 @@
+Vitali = 'Hello! '
+Vassiljev = 'Life! '
+print(Vitali + Vassiljev)
+
+name = input('Enter Your name:')
+print('Hi!', name, '!')
+
+age = input('Enter You age: ')
+print('Great! You are', age, 'years old.')
+
+while True:
+    o = input('Please write your name: ')
+    x = input('Please add your birthday year: ')
+    y = input("Please enter current year: ")
+    Equal = int(y) - int(x)
+
+    print(o, 'You are:', Equal, 'years old.')
+    print('Thank you', o, '!')
